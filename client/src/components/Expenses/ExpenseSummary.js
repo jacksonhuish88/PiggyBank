10e54b9cfe7785390264f42baaa452ae2844cef5
Dexter Stephens <dexterfstephens@gmail.com>
2022-12-28 06:47:30 -0700

    Add the api for the expenses and incomes to get from the database

--- client/src/components/Expenses/ExpenseSummary.js
+++ client/src/components/Expenses/ExpenseSummary.js
@@ -1,6 +1,16 @@
 import '../../App.css';
+import react from "react";
+import axios from "axios";
 
 const ExpenseSummary = () => {
+    let [expenses, setExpenses] = react.useState([]);
+
+    react.useEffect(() => {
+        axios.get("http://localhost:5500/expenses")
+            .then(response => setExpenses(response.data))
+            .catch(err => console.log(err))
+    }, []);
+
     return (
         <div>
                 <h4>EXPENSE SUMMARY</h4>
@@ -11,9 +21,20 @@ const ExpenseSummary = () => {
                             <th scope="col">Expense Name</th>
                             <th scope="col">$/Wk</th>
                             <th scope="col">$/Month</th>
+                            <th scope="col">Notes</th>
                         </tr>
                         </thead>
                         <tbody id='expensesummary'>
+                            {expenses.map(expense => {
+                                return (
+                                    <tr>
+                                        <td>{expense.name}</td>
+                                        <td>{expense.monthly_total / 4}</td>
+                                        <td>{expense.monthly_total}</td>
+                                        <td>{expense.notes}</td>
+                                    </tr>
+                                )
+                            })}
                         </tbody>
                     </table>
                 </div>
--- client/src/components/Incomes/IncomeSummary.js
+++ client/src/components/Incomes/IncomeSummary.js
@@ -1,6 +1,19 @@
 import '../../App.css';
+import axios from "axios";
+import react from "react";
+
 
 const IncomeSummary = () => {
+    let [incomes, setIncomes] = react.useState([]);
+
+    react.useEffect(() => {
+        axios.get("http://localhost:5500/incomes")
+        .then(response => {
+            setIncomes(response.data);
+        })
+        .catch(error => console.log(error))
+    }, []);
+
     return (
         <div>
             <h4>INCOME SUMMARY</h4>
@@ -11,9 +24,23 @@ const IncomeSummary = () => {
                         <th scope="col">Income Name</th>
                         <th scope="col">$/Wk</th>
                         <th scope="col">$/Month</th>
+                        <th scope="col">Hours/Week</th>
+                        <th scope="col">Notes</th>
                     </tr>
                     </thead>
                     <tbody id='incomesummary'>
+                        {incomes.map(income => {
+                            return (
+                                <tr>
+                                    <td>{income.title}</td>
+                                    <td>{income.rate}</td>
+                                    <td>{income.rate * 4}</td>
+                                    <td>{income.weekly_hours}</td>
+                                    <td>{income.notes}</td>
+                                </tr>
+                            )
+                        })
+                        }
                     </tbody>
                 </table>
             </div>
