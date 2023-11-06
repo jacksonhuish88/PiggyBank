6ac45a2c668760442e8899cdfcdc7674a855205b
espilker <ethanspilker@gmail.com>
2023-11-06 08:34:23 -0700

    Added budget field
    Added Budget field and text box to enter information

--- client/src/components/Expenses/ExpenseAccordions/Accordion.js
+++ client/src/components/Expenses/ExpenseAccordions/Accordion.js
@@ -1,29 +1,53 @@
-import React from 'react';
-import '../../../App.css';
-import '../ExpenseAccordions/ExpenseAccordions.css';
+import React, { useState } from "react";
+import "../../../App.css";
+import "../ExpenseAccordions/ExpenseAccordions.css";
 
 function Accordion(props) {
-    const { category, handleDelete } = props;
-    return (
-      <div className="accordion">
-        <div className="accordion-header">
-          {category}
-          <button class='deletebutton' type="button" onClick={handleDelete}>
-            Delete Piggy Bank
-          </button>
-        </div>
+  const { category, handleDelete } = props;
+  const [isEditing, setIsEditing] = useState(false);
+  const [budgetValue, setBudgetValue] = useState(0);
+  const toggleEditingMode = () => {
+    setIsEditing(!isEditing);
+  };
+
+  return (
+    <div className="accordion">
+      <div className="accordion-header">
+        {category}
+        <button class="deletebutton" type="button" onClick={handleDelete}>
+          Delete Piggy Bank
+        </button>
+      </div>
       <div className="accordion-content">
-        <table class='table table-responsive'>
-            <thead>
-                <tr>
-                    <th scope='col'>Check</th>
-                    <th scope='col'>Name</th>
-                    <th scope='col'>$/Month</th>
-                    <th scope='col'>Notes</th>
-                    <th scope='col'>Edit</th>
-                    <th scope='col'>Delete</th>
-                </tr>
-            </thead>
+        <table class="table table-responsive">
+          <thead>
+            <tr>
+              <th scope="col">Check</th>
+              <th scope="col">Name</th>
+              <th scope="col">$/Month</th>
+              <th scope="col">Notes</th>
+              <th scope="col">Budget</th>
+              <th scope="col">Edit</th>
+              <th scope="col">Delete</th>
+            </tr>
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th scope="col">
+                {isEditing ? (
+                  <input
+                    type="number"
+                    value={budgetValue}
+                    onChange={(e) => setBudgetValue(e.target.value)}
+                  />
+                ) : (
+                  <span onClick={toggleEditingMode}>{budgetValue}</span>
+                )}
+              </th>
+            </tr>
+          </thead>
         </table>
       </div>
     </div>
